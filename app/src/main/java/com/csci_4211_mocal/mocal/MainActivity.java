61200b509bf1372aae661794d9ac87b3272da74c
Jonathan Fletcher <jonny.fletcher@icloud.com>
2022-04-02 12:12:55 -0600

    boilerplate

---
+++ app/src/main/java/com/csci_4211_mocal/mocal/MainActivity.java
@@ -0,0 +1,14 @@
+package com.csci_4211_mocal.mocal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
